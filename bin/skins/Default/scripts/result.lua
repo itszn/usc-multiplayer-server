45b2efafb10a8bfeb232a63b98e742f728d0646c
Drewol <emildraws@gmail.com>
2018-09-17 19:31:24 +0200

    Added gauge samples to result lua.

--- bin/skins/Default/scripts/result.lua
+++ bin/skins/Default/scripts/result.lua
@@ -13,5 +13,14 @@ render = function(deltaTime, showStats)
     gfx.Text("SCORE: " .. tostring(result.score), 5, 80);
     gfx.Text("GAUGE: " .. string.format("%1.f", result.gauge * 100) .. "%", 5, 100);
     gfx.Text("BPM: " .. result.bpm, 5, 120);
+    local x = 0;
+    for i = 1, #result.gaugeSamples do
+       local sample = result.gaugeSamples[i];
+       gfx.BeginPath()
+       gfx.Rect(x,240,2,20);
+       x = x + 2;
+       gfx.FillColor(0, math.floor(sample * 255), 0);
+       gfx.Fill()
+    end
     gfx.ImageRect(5,140,100,100,jacket,1,0)
 end
