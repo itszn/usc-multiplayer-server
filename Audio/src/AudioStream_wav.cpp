a3b40f810e303eca614d8a7c647487b571d10421
Drewol <emildraws@gmail.com>
2018-11-26 22:15:05 +0100

    Wav file proper preloading and file streaming

--- Audio/src/AudioStream_wav.cpp
+++ Audio/src/AudioStream_wav.cpp
@@ -32,9 +32,10 @@ class AudioStreamWAV_Impl : public AudioStreamBase
 private:
 	Buffer m_Internaldata;
 	WavFormat m_format = { 0 };
-
+	Vector<float> m_pcm;
 
 	uint64 m_playbackPointer = 0;
+	uint64 m_dataPosition = 0;
 
 	uint32 m_decode_ms_adpcm(const Buffer& encoded, Buffer* decoded, uint64 pos)
 	{
@@ -120,80 +121,200 @@ public:
 
 	bool Init(Audio* audio, const String& path, bool preload)
 	{
-		if (!AudioStreamBase::Init(audio, path, true)) // Always preload for now
+		if (!AudioStreamBase::Init(audio, path, preload)) // Always preload for now
 			return false;
 
-		WavHeader riff;
-		m_memoryReader << riff;
-		if (riff != "RIFF")
-			return false;
-
-		char riffType[4];
-		m_memoryReader.SerializeObject(riffType);
-		if (strncmp(riffType, "WAVE", 4) != 0)
-			return false;
-
-		while (m_memoryReader.Tell() < m_memoryReader.GetSize())
+		if (preload)
 		{
-			WavHeader chunkHdr;
-			m_memoryReader << chunkHdr;
-			if (chunkHdr == "fmt ")
-			{
-				m_memoryReader << m_format;
-				String format = "Unknown";
-				if (m_format.nFormat == 1)
-					format = "PCM";
-				else if (m_format.nFormat == 2)
-					format = "MS_ADPCM";
+			WavHeader riff;
+			m_memoryReader << riff;
+			if (riff != "RIFF")
+				return false;
 
-				Logf("Sample format: %s", Logger::Info, format);
-				Logf("Channels: %d", Logger::Info, m_format.nChannels);
-				Logf("Sample rate: %d", Logger::Info, m_format.nSampleRate);
-				Logf("Bps: %d", Logger::Info, m_format.nBitsPerSample);
-				if (m_format.nFormat == 2)
-				{
-					uint16 cbSize;
-					m_memoryReader << cbSize;
-					m_memoryReader.Skip(cbSize);
-				}
-			}
-			else if (chunkHdr == "fact")
-			{
-				uint32 fh;
-				m_memoryReader << fh;
-				m_samplesTotal = fh;
-			}
-			else if (chunkHdr == "data") // data Chunk
-			{
-				// validate header
-				if (m_format.nFormat != 1 && m_format.nFormat != 2)
-					return false;
-				if (m_format.nChannels > 2 || m_format.nChannels == 0)
-					return false;
-				if (m_format.nBitsPerSample != 16 && m_format.nBitsPerSample != 4)
-					return false;
+			char riffType[4];
+			m_memoryReader.SerializeObject(riffType);
+			if (strncmp(riffType, "WAVE", 4) != 0)
+				return false;
 
-				// Read data
-				if (m_format.nFormat == 1)
+			while (m_memoryReader.Tell() < m_memoryReader.GetSize())
+			{
+				WavHeader chunkHdr;
+				m_memoryReader << chunkHdr;
+				if (chunkHdr == "fmt ")
 				{
-					m_samplesTotal = chunkHdr.nLength / sizeof(short);
-					m_Internaldata.resize(chunkHdr.nLength);
-					m_memoryReader.Serialize(m_Internaldata.data(), chunkHdr.nLength);
+					m_memoryReader << m_format;
+					String format = "Unknown";
+					if (m_format.nFormat == 1)
+						format = "PCM";
+					else if (m_format.nFormat == 2)
+						format = "MS_ADPCM";
+
+					Logf("Sample format: %s", Logger::Info, format);
+					Logf("Channels: %d", Logger::Info, m_format.nChannels);
+					Logf("Sample rate: %d", Logger::Info, m_format.nSampleRate);
+					Logf("Bps: %d", Logger::Info, m_format.nBitsPerSample);
+					if (m_format.nFormat == 2)
+					{
+						uint16 cbSize;
+						m_memoryReader << cbSize;
+						m_memoryReader.Skip(cbSize);
+					}
 				}
-				else if (m_format.nFormat == 2)
+				else if (chunkHdr == "fact")
 				{
-					m_samplesTotal = chunkHdr.nLength;
-					m_Internaldata.resize(m_samplesTotal * m_format.nChannels * sizeof(short));
-					m_memoryReader.Serialize(m_Internaldata.data(), chunkHdr.nLength);
+					uint32 fh;
+					m_memoryReader << fh;
+					m_samplesTotal = fh;
+				}
+				else if (chunkHdr == "data") // data Chunk
+				{
+					// validate header
+					if (m_format.nFormat != 1 && m_format.nFormat != 2)
+						return false;
+					if (m_format.nChannels > 2 || m_format.nChannels == 0)
+						return false;
+					if (m_format.nBitsPerSample != 16 && m_format.nBitsPerSample != 4)
+						return false;
+
+					// Read data
+					if (m_format.nFormat == 1)
+					{
+						m_samplesTotal = chunkHdr.nLength / sizeof(short);
+						m_Internaldata.resize(chunkHdr.nLength);
+						m_memoryReader.Serialize(m_Internaldata.data(), chunkHdr.nLength);
+					}
+					else if (m_format.nFormat == 2)
+					{
+						m_samplesTotal = chunkHdr.nLength * 2;
+						m_Internaldata.resize(m_samplesTotal * m_format.nChannels * sizeof(short));
+						m_memoryReader.Serialize(m_Internaldata.data(), chunkHdr.nLength);
+					}
+
+
+					//Decode to float
+					int pos = 0;
+					m_pcm.resize(m_samplesTotal * sizeof(float));
+					if (m_format.nFormat == 1)
+					{
+						int16* src = ((int16*)m_Internaldata.data());
+						if (m_format.nChannels == 2)
+						{
+							while (2 * pos < m_samplesTotal)
+							{
+								m_pcm[pos * 2] = (float)src[pos * 2] / (float)0x7FFF;
+								m_pcm[pos * 2 + 1] = (float)src[pos * 2 + 1] / (float)0x7FFF;
+								pos++;
+							}
+						}
+						else if (m_format.nChannels == 1)
+						{
+							while (pos < m_samplesTotal)
+							{
+								m_pcm[pos * 2] = (float)src[pos] / (float)0x7FFF;
+								m_pcm[pos * 2 + 1] = (float)src[pos] / (float)0x7FFF;
+								pos++;
+							}
+						}
+					}
+					else if (m_format.nFormat == 2)
+					{
+						int decoded = 0;
+						Buffer decodedBuffer;
+						decodedBuffer.resize(m_format.nBlockAlign * m_format.nChannels * sizeof(short));
+						
+						while (pos < m_samplesTotal)
+						{
+							int newDecoded = m_decode_ms_adpcm(m_Internaldata, &decodedBuffer, pos);
+							pos += m_format.nBlockAlign;
+							int16* src = ((int16*)decodedBuffer.data());
+							for (size_t i = 0; i < newDecoded; i++)
+							{
+								m_pcm[(2 * decoded) + (i * 2)] = (float)src[i * 2] / (float)0x7FFF;
+								m_pcm[(2 * decoded) + (i * 2) + 1] = (float)src[i * 2 + 1] / (float)0x7FFF;
+							}
+							decoded += newDecoded;
+						}
+
+						m_samplesTotal = decoded;
+					}
+				}
+				else
+				{
+					m_memoryReader.Skip(chunkHdr.nLength);
 				}
 			}
-			else
+			m_Internaldata.clear();
+		}
+		else
+		{
+			WavHeader riff;
+			m_fileReader << riff;
+			if (riff != "RIFF")
+				return false;
+
+			char riffType[4];
+			m_fileReader.SerializeObject(riffType);
+			if (strncmp(riffType, "WAVE", 4) != 0)
+				return false;
+
+			while (m_fileReader.Tell() < m_fileReader.GetSize())
 			{
-				m_memoryReader.Skip(chunkHdr.nLength);
+				WavHeader chunkHdr;
+				m_fileReader << chunkHdr;
+				if (chunkHdr == "fmt ")
+				{
+					m_fileReader << m_format;
+					String format = "Unknown";
+					if (m_format.nFormat == 1)
+						format = "PCM";
+					else if (m_format.nFormat == 2)
+						format = "MS_ADPCM";
+
+					Logf("Sample format: %s", Logger::Info, format);
+					Logf("Channels: %d", Logger::Info, m_format.nChannels);
+					Logf("Sample rate: %d", Logger::Info, m_format.nSampleRate);
+					Logf("Bps: %d", Logger::Info, m_format.nBitsPerSample);
+					if (m_format.nFormat == 2)
+					{
+						uint16 cbSize;
+						m_fileReader << cbSize;
+						m_fileReader.Skip(cbSize);
+					}
+				}
+				else if (chunkHdr == "fact")
+				{
+					uint32 fh;
+					m_fileReader << fh;
+					m_samplesTotal = fh;
+				}
+				else if (chunkHdr == "data") // data Chunk
+				{
+					// validate header
+					if (m_format.nFormat != 1 && m_format.nFormat != 2)
+						return false;
+					if (m_format.nChannels > 2 || m_format.nChannels == 0)
+						return false;
+					if (m_format.nBitsPerSample != 16 && m_format.nBitsPerSample != 4)
+						return false;
+
+					if (m_format.nFormat == 1)
+					{
+						m_samplesTotal = chunkHdr.nLength / sizeof(short);
+					}
+					else if (m_format.nFormat == 2)
+					{
+						m_samplesTotal = chunkHdr.nLength * 2;
+					}
+					m_dataPosition = m_fileReader.Tell();
+				}
+				else
+				{
+					m_fileReader.Skip(chunkHdr.nLength);
+				}
 			}
 		}
-		InitSampling(m_format.nSampleRate);
 
+		InitSampling(m_format.nSampleRate);
 		m_playbackPointer = 0;
 		return true;
 	}
@@ -203,7 +324,7 @@ public:
 	}
 	virtual int32 GetStreamRate_Internal()
 	{
-		if (m_format.nFormat == 2)
+		if (m_format.nFormat == 2 && !m_preloaded)
 			return m_format.nByteRate;
 		else
 			return m_format.nSampleRate * m_format.nChannels;
@@ -214,75 +335,119 @@ public:
 			m_playbackPointer = pos;
 		else
 			m_playbackPointer = 0;
+
+		if (!m_preloaded)
+		{
+			int filePos = 0;
+			if (m_format.nFormat == 1)
+			{
+				filePos = m_dataPosition + pos * sizeof(short);
+			}
+			else if (m_format.nFormat == 2)
+			{
+				filePos = m_dataPosition + pos - (pos % m_format.nBlockAlign);
+				//filePos -= filePos % m_format.nBlockAlign;
+			}
+			m_fileReader.Seek(filePos);
+		}
 	}
 
 	virtual int32 DecodeData_Internal()
 	{
-		if (m_format.nFormat == 1)
+		if (!m_preloaded)
 		{
-			uint32 samplesPerRead = 128;
-
-			if (m_format.nChannels == 2)
+			if (m_format.nFormat == 1)
 			{
-				for (uint32 i = 0; i < samplesPerRead; i++)
+				uint32 samplesPerRead = 128;
+				Buffer readData;
+				readData.resize(samplesPerRead * m_format.nChannels * sizeof(short));
+				m_fileReader.Serialize(readData.data(), samplesPerRead * m_format.nChannels * sizeof(short));
+				int16* src = ((int16*)readData.data());
+				if (m_format.nChannels == 2)
 				{
-					if (m_playbackPointer >= m_samplesTotal)
+					for (uint32 i = 0; i < samplesPerRead; i++)
 					{
-						m_currentBufferSize = samplesPerRead;
-						m_remainingBufferData = samplesPerRead;
-						return i;
-					}
+						if (m_playbackPointer >= m_samplesTotal)
+						{
+							m_currentBufferSize = samplesPerRead;
+							m_remainingBufferData = samplesPerRead;
+							return i;
+						}
 
-					int16* src = ((int16*)m_Internaldata.data()) + m_playbackPointer;
+						m_readBuffer[0][i] = (float)src[i * 2] / (float)0x7FFF;
+						m_readBuffer[1][i] = (float)src[i * 2 + 1] / (float)0x7FFF;
+						m_playbackPointer += 2;
+					}
+				}
+				else
+				{
+					// Mix mono sample
+					for (uint32 i = 0; i < samplesPerRead; i++)
+					{
+						if (m_playbackPointer >= m_samplesTotal)
+						{
+							m_currentBufferSize = samplesPerRead;
+							m_remainingBufferData = samplesPerRead;
+							return i;
+						}
+						m_readBuffer[0][i] = (float)src[0] / (float)0x7FFF;
+						m_readBuffer[1][i] = (float)src[0] / (float)0x7FFF;
+						m_playbackPointer++;
+					}
+				}
+				m_currentBufferSize = samplesPerRead;
+				m_remainingBufferData = samplesPerRead;
+				return samplesPerRead;
+			}
+			else if (m_format.nFormat == 2)
+			{
+				Buffer decoded;
+				decoded.resize(m_format.nBlockAlign * m_format.nChannels * sizeof(short));
+				Buffer readData;
+				readData.resize(m_format.nBlockAlign);
+				int amountRead = m_fileReader.Serialize(readData.data(), m_format.nBlockAlign);
+				if (amountRead < m_format.nBlockAlign)
+				{
+					m_ended = true;
+					return 0;
+				}
+				uint32 decodedCount = m_decode_ms_adpcm(readData, &decoded, 0);
+				uint32 samplesInserted = 0;
+				uint64 bufferOffset = 0;
+				for (uint32 i = 0; i < decodedCount; i++)
+				{
+					int16* src = ((int16*)decoded.data()) + (i * 2);
 					m_readBuffer[0][i] = (float)src[0] / (float)0x7FFF;
 					m_readBuffer[1][i] = (float)src[1] / (float)0x7FFF;
-					m_playbackPointer+=2;
-
 				}
+
+				m_playbackPointer += m_format.nBlockAlign;
+
+				m_currentBufferSize = decodedCount;
+				m_remainingBufferData = decodedCount;
+				return decodedCount;
+
 			}
-			else
+		}
+		else
+		{
+			uint32 samplesPerRead = 128;
+			for (uint32 i = 0; i < samplesPerRead; i++)
 			{
-				// Mix mono sample
-				for (uint32 i = 0; i < samplesPerRead; i++)
+				if (m_playbackPointer >= m_samplesTotal)
 				{
-					if (m_playbackPointer >= m_samplesTotal)
-					{
-						m_currentBufferSize = samplesPerRead;
-						m_remainingBufferData = samplesPerRead;
-						return i;
-					}
-
-					int16* src = ((int16*)m_Internaldata.data()) + m_playbackPointer;
-					m_readBuffer[0][i] = (float)src[0] / (float)0x7FFF;
-					m_readBuffer[1][i] = (float)src[0] / (float)0x7FFF;
-					m_playbackPointer++;
+					m_currentBufferSize = samplesPerRead;
+					m_remainingBufferData = samplesPerRead;
+					return i;
 				}
+
+				m_readBuffer[0][i] = m_pcm[m_playbackPointer];
+				m_readBuffer[1][i] = m_pcm[m_playbackPointer + 1];
+				m_playbackPointer += 2;
 			}
 			m_currentBufferSize = samplesPerRead;
 			m_remainingBufferData = samplesPerRead;
 			return samplesPerRead;
-		}
-		else if (m_format.nFormat == 2)
-		{
-			Buffer decoded;
-			m_playbackPointer -= m_playbackPointer % m_format.nBlockAlign;
-			decoded.resize(m_format.nBlockAlign * m_format.nChannels * sizeof(short));
-			uint32 decodedCount = m_decode_ms_adpcm(m_Internaldata, &decoded, m_playbackPointer);
-			uint32 samplesInserted = 0;
-			uint64 bufferOffset = 0;
-			for (uint32 i = 0; i < decodedCount; i++)
-			{
-				int16* src = ((int16*)decoded.data()) + (i * 2);
-				m_readBuffer[0][i] = (float)src[0] / (float)0x7FFF;
-				m_readBuffer[1][i] = (float)src[1] / (float)0x7FFF;
-			}
-			
-			m_playbackPointer += m_format.nBlockAlign;
-
-			m_currentBufferSize = decodedCount;
-			m_remainingBufferData = decodedCount;
-			return decodedCount;
-
 		}
 		return 0;
 	}
