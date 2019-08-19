82946bc7987d3848f310b9a30dbd557d562bd118
itszn <itszn@itszn.com>
2019-08-18 21:09:49 -0400

    Make logging a little quieter

--- src/app.go
+++ src/app.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"log"
+	"os"
 
 	"github.com/ThreeDotsLabs/watermill"
 )
 
 var (
-	logger = watermill.NewStdLogger(false, false)
+	logger watermill.LoggerAdapter
 )
 
 var (
@@ -18,20 +21,29 @@ var (
 
 func main() {
 	f_VERBOSE := flag.Bool("verbose", false, "Print verbose info")
+	f_DEBUG := flag.Bool("debug", false, "Print debug info")
 	f_SCOREBOARD_REFERSH_RATE := flag.Int("refresh", 500, "Number of milliseconds between score updates (0 for realtime)")
 
 	var bind string
 	flag.StringVar(&bind, "bind", "0.0.0.0:39079", "host:port to bind to")
-
 	flag.Parse()
+
+	// Setup watermill logging
+	logger_impl := log.New(os.Stderr, "[watermill] ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
 
 	if *f_VERBOSE {
 		DEBUG_LEVEL = 3 // Verbose
+		logger = &watermill.StdLoggerAdapter{ErrorLogger: logger_impl, InfoLogger: logger_impl, DebugLogger: logger_impl}
+	} else if *f_DEBUG {
+		DEBUG_LEVEL = 2 // Debug
+		logger = &watermill.StdLoggerAdapter{ErrorLogger: logger_impl, InfoLogger: logger_impl}
 	} else {
 		DEBUG_LEVEL = 1 // Info
+		logger = &watermill.StdLoggerAdapter{ErrorLogger: logger_impl}
 	}
 	SCOREBOARD_REFERSH_RATE = *f_SCOREBOARD_REFERSH_RATE
 
+	fmt.Println("Starting server on", bind)
 	server := New_server(bind)
 	server.Start()
 }
